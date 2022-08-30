62f87de5f9ee89a1b5191fae62c4a521794cee0b
Sergey Kim <Sergeykim486@gmail.com>
2022-08-31 00:48:02 +0500

    Задача 2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿try
+    {
+    Console.WriteLine("Введите первое целое число...");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе целое число...");
+    int b = Convert.ToInt32(Console.ReadLine());
+    if (a>=b)
+        Console.WriteLine("максимальное число; "+a);
+    else
+        Console.WriteLine("максимальное число; "+b);
+    }
+
+catch
+    {
+    Console.WriteLine("Вы ввели не целое число!!!");
+    }
