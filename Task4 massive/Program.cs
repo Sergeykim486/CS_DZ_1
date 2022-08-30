e086a407a0351925ff70046d00fd2b85cd08c149
Sergey Kim <Sergeykim486@gmail.com>
2022-08-31 01:54:44 +0500

    Задача 4 решение с помощью массива на неограниченное кол-во чисел

---
+++ Task4 massive/Program.cs
@@ -0,0 +1,34 @@
+﻿try
+{
+        Console.WriteLine("Сколько чисел сравниваем?");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int [] numbers = new int[n];
+        int i = 0;
+        int max = 0;
+    
+        while (i<n)
+        {
+            int a = i + 1;
+            Console.WriteLine("Введите целое число №"+a);
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+            i++;
+        }
+
+        i = 1;
+        while (i<n)
+        {
+            if (numbers[i] > numbers[max])
+            {
+                max = i;
+            }
+            i++;
+        }
+
+        Console.WriteLine("Максимальное число: "+numbers[max]);
+}
+
+
+catch
+{
+    Console.WriteLine("Вы ввели не целое число!!!");
+}
