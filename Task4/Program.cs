b5ae8a6480184683f2d46203f45994df8d63d0f3
Sergey Kim <Sergeykim486@gmail.com>
2022-08-31 01:13:07 +0500

    Задача 4 решение условием

---
+++ Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿try
+    {
+    Console.WriteLine("Введите первое целое число...");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе целое число...");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите третье целое число...");
+    int c = Convert.ToInt32(Console.ReadLine());
+    int max = a;
+
+    if (b>=max)
+        max = b;
+    if (c>=max)
+        max = c;
+
+    Console.WriteLine("Максимальное число: "+max);
+    }
+
+catch
+    {
+    Console.WriteLine("Вы ввели не целое число!!!");
+    }
